5f38c01445ad371d23c2ac85c7196d0eb66f3c06
pedrovnascimento <pedrovnascimento@gmail.com>
2017-04-16 15:21:35 -0300

    usa classe RFIDReader para conectar
    substitui o uso da classe teste pela classe RFID.
    Falta o uso da classe RFIDAutonomous

--- src/br/ufmg/dcc/iot/binding/JAXBDelegate.java
+++ src/br/ufmg/dcc/iot/binding/JAXBDelegate.java
@@ -10,7 +10,14 @@ import javax.xml.bind.Unmarshaller;
 import br.ufmg.dcc.iot.binding.beans.MetricsManagerXMLBind;
 import br.ufmg.dcc.iot.binding.converter.MetricsManagerBinder;
 import br.ufmg.dcc.iot.business.MetricsManager;
+import br.ufmg.dcc.iot.business.ReadAttempt;
+import br.ufmg.dcc.iot.business.ReadResult;
+import br.ufmg.dcc.iot.business.common.ReaderConn;
+import br.ufmg.dcc.iot.reader.rfid.RFIDAutonomousReader;
+import br.ufmg.dcc.iot.reader.rfid.RFIDReader;
 import br.ufmg.dcc.iot.reader.test.TestReader;
+
+import com.alien.enterpriseRFID.tags.*;
 
 public class JAXBDelegate {
 
@@ -28,16 +35,54 @@ public class JAXBDelegate {
 		return MetricsManagerBinder.convert(bean); 
 	}	
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
+		
+		ReaderConn connection = ReaderConn.createALR9900Conn(); 
+		
+		
+		RFIDReader leitorRFID = new RFIDReader(connection);
+		//RFIDAutonomousReader leitorRFID = new RFIDAutonomousReader(connection);
+		
+		leitorRFID.connect();
+		ReadAttempt tentativaLeitura;
+		
+		/*
+		//ReadAttempt tentativaLeitura = leitorRFID.read();
+		
+		
+		Tag tagList[] = tentativaLeitura.getResult().getTags();
+		
+		if (tagList == null) {
+			//readingResult.incNoTagCount();
+		} else {
+			for (int i = 0; i < tagList.length; i++) {
+				Tag tag = tagList[i];
+				System.out.println("ID:" + tag.getTagID() +
+                         ", Discovered:" + tag.getDiscoverTime() +
+                         ", Last Seen:" + tag.getRenewTime() +
+                         ", Antenna:" + tag.getAntenna() +
+                         ", Reads:" + tag.getRenewCount()
+                         );
+			}
+		}
+		
+		
+		
+		System.out.println(leitorRFID.isOpen());
+		*/
+		
 		
 		TestReader reader = null;
 		reader = new TestReader(null);
 		MetricsManager manager = new MetricsManager();
 		
-		int i = 200;
+		int i = 1;
 		while(i > 0) {
-			manager.addResult(reader.read());
+			tentativaLeitura = leitorRFID.read();
+			manager.addResult(tentativaLeitura);
 			i--;
+			System.out.println(manager.getReadRate());
+			
 		}		
 		File file = new File("file.xml");
 		try{
